33bb5681001594df80aad0f1a31aaab05092b5af
Ben Hunt <wbh26@txstate.edu>
2018-03-29 00:37:51 -0500

    Should be able to run process 1 video for training input

--- VideoProcessing/Main.py
+++ VideoProcessing/Main.py
@@ -5,6 +5,7 @@ import VideoProcessor
 def Main():
 	print("hello")
 	vidProc = VideoProcessor.VideoProcessor()
+	vidProc.process_video_for_training()
 
 
 
--- VideoProcessing/VideoProcessor.py
+++ VideoProcessing/VideoProcessor.py
@@ -18,7 +18,7 @@ class VideoProcessor:
 
 	def __init__(self,
 					video_path = "./ADL_Dataset/P_01.mp4",
-					action_list_path = "action_annotation/overlap_labels.txt",
+					action_list_path = "action_annotation/overlap_labels_final.txt",
 					action_annotation_path = "action_annotation/P_01.txt",
 					num_frames = 64,):
 
@@ -26,7 +26,7 @@ class VideoProcessor:
 		self.action_list_path = action_list_path
 		self.action_annotation_path = action_annotation_path
 		self.num_frames = 64
-		self.action_list = [] # Will populate with the action label
+		self.action_list = {} # Will populate with the action label
 
 
 
@@ -34,20 +34,24 @@ class VideoProcessor:
 		#Array index corresponds to label index, string corresponds name of label
 		with open(action_list_path) as fin:
 			for line in fin.readlines():
-				self.action_list.append(line.split("'")[1])
+				label = line.split("(")[1].split(")")[0] # Get the label
+				self.action_list[int(label)] = line.split("'")[1]
 
-		for action in self.action_list:
-			print(action)
+		# for debugging
+		# for key, value in self.action_list.items():
+		# 	print(str(key) + " " + str(value))
 
-		annotations = self.get_annotations()
+		self.annotations = self.get_annotations()
+
+
 
 
 
 	#Creates Directory with all the videos processed as RGB + Flowx + Flowy at the frame rate specified
 	#in the constructor
 	def process_for_I3D_input():
+		
 		return 0
-
 
 
 	#Returns named tuple of the annotations file
@@ -68,7 +72,7 @@ class VideoProcessor:
 
 				start_time = 0
 				end_time = 0
-				label = ""
+				label = int(temp[2])
 
 				start_time += (int(temp[0][0])*10 + int(temp[0][1]))*60
 				start_time += (int(temp[0][3])*10 + int(temp[0][4]))
@@ -76,31 +80,21 @@ class VideoProcessor:
 				end_time += (int(temp[1][0])*10 + int(temp[1][1]))*60
 				end_time += (int(temp[1][3])*10 + int(temp[1][4]))
 
-				if(len(self.action_list) <= int(temp[2]))
-				label = self.action_list[int(temp[2])-1] #Account for index starting at 0
+				if(label in self.action_list):
+					label = self.action_list[label] #Account for index starting at 0
+					annotations.append(Annotation(start_time, end_time, label))
 
-				annotations.append(Annotation(start_time, end_time, label))
-
-		# For debugging
+		#For debugging
 		# for annotation in annotations:
 		# 	print(str(annotation.start_time) + " " + str(annotation.end_time) + " " + annotation.label)
 
-
-		#List of named tuples
-		return annotations
+		return self.get_annotations
 
 
 
 
-	def processVideo():
+	def process_video_for_training(self):
 
-
-		vidCap = cv2.VideoCapture()
-
-
-
-	 #code here
-	def Main():
 
 		successFlag = True
 
@@ -108,10 +102,10 @@ class VideoProcessor:
 		RGB_Input = np.zeros((1,64,224,224,3))
 		FLOW_Input = np.zeros((1,64,224,224,2))
 
-		vidCap = cv2.VideoCapture("Razan washing dishes.mp4")
+		vidCap = cv2.VideoCapture(self.video_path)
 
 		# Get to correct starting frame
-		skipTillSegment(21,vidCap,30)
+		self.skipTillSegment(21,vidCap,30)
 
 		frameNum = 0
 
@@ -159,8 +153,8 @@ class VideoProcessor:
 		np.save('examples/outFlow.npy', FLOW_Input)
 		np.save('examples/outRGB.npy', RGB_Input)
 
-
-
+	def process_video_for_evaluation():
+		return 0
 
 
 
@@ -241,7 +235,7 @@ class VideoProcessor:
 	#startingTime - when in the video the segment starts (pos. integer)
 	#vidCap - the inputstream of video from a file.
 	#fps - the fps of the video, to calculate how many frames to skip
-	def skipTillSegment(startingTime, vidCap, fps):
+	def skipTillSegment(self,startingTime, vidCap, fps):
 
 		currentFrame = 0
 
